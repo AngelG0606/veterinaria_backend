bb0c2caf74c73b6882df8e09e642639b3e1e888e
AngelGutierrez06Dev <angel.gutierrez.razo06@gmail.com>
2025-07-31 21:26:16 -0600

    Avances controlador usuario

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,17 @@
 import { Request, Response} from 'express'
 import User from '../models/User'
 import { comparePassword, hashPassword } from '../utils/hashPassword'
+import { generateToken } from '../utils/jwt'
 
 export class UserController {
     
 
     static createUser = async (req : Request, res : Response) => {
-        const {email } = req.body
-        const userExist = User.findOne({
-            where : email
+        const { email } = req.body
+        const userExist = await User.findOne({
+            where : {
+                email
+            }
         })
         if(userExist) {
             const error = new Error('Usuario ya existente')
@@ -20,7 +23,6 @@ export class UserController {
             
             const user = new User(req.body)
             user.password = await hashPassword(req.body.password)
-
 
             await user.save()
 
@@ -51,7 +53,8 @@ export class UserController {
                 res.status(401).json({error : error.message})
                 return
             }
-            res.json('Cuenta Autenticada Correctamente')
+            const token = await generateToken(user.id)
+            res.send(token)
 
         } catch (error) {
             console.log(error)
@@ -59,8 +62,87 @@ export class UserController {
         }
     }
 
+    static getAll = async (req : Request, res : Response) => {
+        const users = await User.findAll()
+        res.json(users)
+    }
 
+    static getById = async (req : Request, res : Response) => {
+        try {
+            const { userId } = req.params
 
+            const user = await User.findByPk(userId)
+            if(!user) {
+                const error = new Error('Usuario no encontrado')
+                res.status(401).json({error : error.message})
+                return
+            }
+            res.json(user)
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error : 'Hubo un error'})
+        }
+    }
+
+    static editUser = async (req : Request, res : Response) => {
+        try {
+            const { userId } = req.params
+            const user = await User.findByPk(userId)
+            if(!user) {
+                const error = new Error('Usuario no encontrado')
+                res.status(401).json({error : error.message})
+                return
+            }
+
+            user.update(req.body)
+            res.status(201).json('Información actualizada correctamente')
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error : 'Hubo un error'})
+        }
+    }
+
+    static deleteUser = async (req :  Request, res : Response) => {
+        try {
+            const { userId } = req.params
+            const user = await User.findByPk(userId)
+            if(!user) {
+                const error = new Error('Usuario no encontrado')
+                res.status(401).json({error : error.message})
+                return
+            }
+
+            user.destroy()
+            res.status(201).json('Usuario Eliminado Correctamente')
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error : 'Hubo un error'})
+        }
+    }
+
+    static user = async (req : Request, res : Response) => {
+        res.json(req.user)
+    }
+
+    static updatePassword = async (req : Request, res : Response) => {
+        try {
+            const user = await User.findByPk(req.user.id)
+            if(!user) {
+                const error = new Error('Usuario no encontrado')
+                res.status(401).json({error : error.message})
+                return
+            }
+            const {} = req.body
+            
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error : 'Hubo un error'})
+        }
+    }
 
 
 }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { UserController } from '../controllers/UserController'
 import {body, param} from 'express-validator'
 import { handleInputErrors } from '../middleware/validation'
+import { authenticateUser } from '../middleware/authenticateUser'
 
 const router = Router()
 
@@ -26,6 +27,30 @@ router.post('/login',
     body('password').notEmpty().withMessage('El Password del usuario es obligatorio'),
     handleInputErrors, 
     UserController.login
+)
+
+router.get('/users', 
+    UserController.getAll
+)
+
+router.get('/user/:userId',
+    param('userId').isInt().withMessage('ID no válido').custom(value  => value > 0).withMessage('ID no válido'),
+    UserController.getById
+)
+
+router.get('/user',
+    authenticateUser,
+    UserController.user
+)
+
+router.put('/user/:userId',
+    authenticateUser,
+    body('name').notEmpty().withMessage('El Nombre del Usuario es obligatorio'),
+    body('lastname').notEmpty().withMessage('Los Apellidos del Usuario es obligatorio'),
+    body('telefono').notEmpty().withMessage('El Telefono del Usuario es obligatorio'),
+    body('email').isEmail().withMessage('E-mail no válido'),
+    handleInputErrors,
+    UserController.editUser
 )
 
 export default router
